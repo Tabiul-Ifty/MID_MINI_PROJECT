78668b59127c28456d1239364df926efc9eabfd8
tabiulifty <tabiulifty@gmail.com>
2020-08-10 13:28:23 +0600

    .

--- regcheck.php
+++ regcheck.php
@@ -1,51 +1,30 @@
 <?php
+
+	
 	session_start();
+	
+	if(isset($_POST['submit'])){
 
-	if(isset($_SESSION['status'])){
-		if($_SESSION['status'] == "OK"){
+		$uname 		= $_POST['uname'];
+		$email 		= $_POST['email'];
+		$password 	= $_POST['password'];
 
-			$connection = mysqli_connect('127.0.0.1', 'root', '', 'webtech'); 
-			$result = mysqli_query($connection, "select * from users");
+		$sql = "INSERT INTO User (uname,email,password)
+	 	VALUES ('$uname','$email','$password')";
 
-?>
+	 	if (mysqli_query($conn, $sql)) {
+				echo "New record created successfully !";
+			 } else {
+				echo "Error: " . $sql . " " . mysqli_error($conn);
+			 }
+			 mysqli_close($con);
 
+			}	
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-	<h1>Welcome home</h1><a href="logout.php"> logout</a>
-
-	<h3>User List</h3>
-	<table border=1> 
-				<tr>
-					<td>ID</td> 
-					<td>NAME</td>
-					<td>password</td> 
-					<td>Email</td>
-				</tr>
-			<?php while ($row = mysqli_fetch_assoc($result)) { ?>
-				
-				<tr>
-					<td><?= $row['id'] ?></td>
-					<td><?= $row['username'] ?></td>
-					<td><?= $row['password'] ?></td>
-					<td><?= $row['email'] ?> </td>
-				</tr>
-
-			<?php } ?>
-		</table>
-</body>
-</html>
-
-
-<?php
-}else{
-		header('location: login.php');
-		}
 	}else{
-		header('location: login.php');
+		//echo "invalid request";
+		header('location: login.html');
 	}
+
+
 ?>
